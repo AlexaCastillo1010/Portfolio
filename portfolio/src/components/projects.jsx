daf4ecef2674edcb2ea05014d823654617751b4e
Alexa Castillo <alexandriacastillo657@gmail.com>
2025-01-13 18:21:07 +0800

    with projects part

--- portfolio/src/components/projects.jsx
+++ portfolio/src/components/projects.jsx
@@ -1,10 +1,18 @@
 import "../index.css"
+import "./css/projects.css";
 import codecraftimg from "./image/codecraftimg.png"
+import carimg from "./image/car.png"
+import coffeeimg from "./image/coffee.png"
 export default function Projects(){
     return(
         <div className="projects">
             <div className="codecraft">
                 <img src={codecraftimg} alt="" />
+            </div>
+            <div className="latestprojects">
+                <h1 className="latest">Latest <br />Projects</h1>
+                <img src={carimg} alt="" />
+                <img src={coffeeimg} alt="" />
             </div>
         </div>
     );
