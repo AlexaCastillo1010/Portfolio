ae3502ffc18dc59a6fbeba5ac3e7f3b5c67587ab
Alexa Castillo <alexandriacastillo657@gmail.com>
2025-01-15 00:18:10 +0800

    animation and latest project hover

--- portfolio/src/components/projects.jsx
+++ portfolio/src/components/projects.jsx
@@ -3,17 +3,30 @@ import "./css/projects.css";
 import codecraftimg from "./image/codecraftimg.png"
 import carimg from "./image/car.png"
 import coffeeimg from "./image/coffee.png"
-export default function Projects(){
-    return(
-        <div className="projects">
-            <div className="codecraft">
-                <img src={codecraftimg} alt="" />
-            </div>
-            <div className="latestprojects">
-                <h1 className="latest">Latest <br />Projects</h1>
-                <img src={carimg} alt="" />
-                <img src={coffeeimg} alt="" />
-            </div>
+export default function Projects() {
+    return (
+      <div className="projects">
+        <div className="codecraft">
+          <div className="image-container">
+            <img src={codecraftimg} alt="CodeCraft Project" />
+            <span>CodeCraft</span>
+          </div>
         </div>
+        <div className="latestprojects">
+          <h1 className="latest">
+            Latest <br />
+            <span className="font-bold">Projects</span>
+          </h1>
+          <div className="image-container">
+            <img src={carimg} alt="Car Rental" />
+            <span>Car Rental</span>
+          </div>
+          <div className="image-container">
+            <img src={coffeeimg} alt="Coffee Shop" />
+            <span>Coffee Shop</span>
+          </div>
+        </div>
+      </div>
     );
-}
+  }
+  
