7cbb51425f624e780dc2df99199c4d1868a51bb9
Alexa Castillo <alexandriacastillo657@gmail.com>
2025-01-16 20:55:51 +0800

    pdf changes

--- src/components/aboutskills.jsx
+++ src/components/aboutskills.jsx
@@ -15,13 +15,19 @@ export default function AboutSkills() {
             </div>
             <div className="aboutskillsdetails">
                 {/* Download Button */}
-                <a 
-                    href="./MaAlexandriaCastilloIntern.pdf.pdf"
-                    download 
+                <button 
+                    onClick={() => {
+                        const link = document.createElement('a');
+                        link.href = './Ma. Alexandria Castillo Intern.pdf';
+                        link.download = 'Ma. Alexandria Castillo Intern.pdf';
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                    }}
                     className="download"
                 >
                     DOWNLOAD MY RESUME
-                </a>
+                </button>
                 <p className="iam">
                     I am a fourth-year Business Analytics student at Batangas State University - JPLPC Malvar and a self-taught programmer with four years of experience, mainly in web development. I also have a background in creating Windows applications and enjoy exploring new technologies to enhance my skills.
                 </p>
