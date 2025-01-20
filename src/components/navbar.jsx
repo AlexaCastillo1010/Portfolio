bbf6f6b8076bdb3fed0c4ec867c456d307620e3f
Alexa Castillo <alexandriacastillo657@gmail.com>
2025-01-20 14:21:18 +0800

    nav changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,9 @@
 import "../index.css"
+import "./css/navbar.css";
 export default function NavBar() {
     return (
-        <div className="flex items-center justify-between border border-black p-5 shadow-md mt-2">
-            <h1 className="flex items-center justify-start text-4xl font-bold">AXC</h1>
+        <div className="container flex items-center justify-between border border-black p-5 shadow-md mt-2">
+            <h1 className="axc flex items-center justify-start text-4xl font-bold">AXC</h1>
 
             <div className="navchoices flex-1 text-center">
             <nav className="flex items-center justify-center">
@@ -11,7 +12,7 @@ export default function NavBar() {
                 </ul>
             </nav>
             </div>
-            <h1 className="flex justify-end">PH Tanauan Batangas</h1>
+            <h1 className="pH flex justify-end">PH Tanauan Batangas</h1>
         </div>
 
     );
