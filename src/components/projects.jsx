9978626920ba53866b19edcd629c164065bf7e78
Alexa Castillo <alexandriacastillo657@gmail.com>
2025-01-21 08:48:37 +0800

    hover effect on projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -26,13 +26,20 @@ export default function Projects() {
   return (
     <div className="projects">
       <div className="codecraft animate-on-scroll">
-        <div className="image-container">
+        <div className="image-container relative group">
           <a
             href="https://codecrafts.online/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={codecraftimg} alt="CodeCraft Project" />
+            <img
+              src={codecraftimg}
+              alt="CodeCraft Project"
+              className="transition-opacity duration-300 group-hover:opacity-70"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              Visit CodeCraft
+            </div>
           </a>
           <span>CodeCraft</span>
         </div>
@@ -42,12 +49,38 @@ export default function Projects() {
           Latest <br />
           <span className="font-bold">Projects</span>
         </h1>
-        <div className="image-container animate-on-scroll">
-          <img src={carimg} alt="Car Rental" />
+        <div className="image-container animate-on-scroll relative group">
+          <a
+            href="https://github.com/AlexaCastillo1010/CarRental.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={carimg}
+              alt="Car Rental"
+              className="transition-opacity duration-300 group-hover:opacity-70"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              Car Rental
+            </div>
+          </a>
           <span>Car Rental</span>
         </div>
-        <div className="image-container animate-on-scroll">
-          <img src={coffeeimg} alt="Coffee Shop" />
+        <div className="image-container animate-on-scroll relative group">
+          <a
+            href="https://github.com/AlexaCastillo1010/CoffeeVerse.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={coffeeimg}
+              alt="Coffee Shop"
+              className="transition-opacity duration-300 group-hover:opacity-70"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              Coffee Shop
+            </div>
+          </a>
           <span>Coffee Shop</span>
         </div>
       </div>
--- src/components/techstack.jsx
+++ src/components/techstack.jsx
@@ -1,48 +1,81 @@
+import React, { useEffect, useRef } from "react";
 import reactimg from "./image/reactimg.png";
-import reduximg from "./image/redux.png"
-import htmlimg from "./image/html.png"
-import mongoimg from "./image/mongodb.png"
-import nodeimg from "./image/node.png"
-import phpimg from "./image/php.png"
-import expressimg from "./image/express.png"
-import jsimg from "./image/js.png"
-import cssimg from "./image/css3.png"
-import "./css/techstack.css"
+import reduximg from "./image/redux.png";
+import htmlimg from "./image/html.png";
+import mongoimg from "./image/mongodb.png";
+import nodeimg from "./image/node.png";
+import phpimg from "./image/php.png";
+import expressimg from "./image/express.png";
+import jsimg from "./image/js.png";
+import cssimg from "./image/css3.png";
+import "./css/techstack.css";
+
 export default function Techstack() {
-    return(
+    const rowsRef = useRef([]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("visible");
+                    } else {
+                        entry.target.classList.remove("visible");
+                    }
+                });
+            },
+            { threshold: 0.2 } // Trigger when 20% of the element is visible
+        );
+
+        rowsRef.current.forEach((row) => {
+            if (row) {
+                observer.observe(row);
+            }
+        });
+
+        return () => {
+            rowsRef.current.forEach((row) => {
+                if (row) {
+                    observer.unobserve(row);
+                }
+            });
+        };
+    }, []);
+
+    return (
         <div className="techstack">
             <h1 className="my">My Tech Stack</h1>
-            <div className="firstrow">
+            <div className="firstrow hidden" ref={(el) => (rowsRef.current[0] = el)}>
                 <div className="react">
-                    <img src={reactimg} alt="" />
+                    <img src={reactimg} alt="React" />
                 </div>
                 <div className="redux">
-                    <img src={reduximg} alt="" />
+                    <img src={reduximg} alt="Redux" />
                 </div>
                 <div className="html">
-                    <img src={htmlimg} alt="" />
+                    <img src={htmlimg} alt="HTML" />
                 </div>
             </div>
-            <div className="secondrow">
+            <div className="secondrow hidden" ref={(el) => (rowsRef.current[1] = el)}>
                 <div className="mongodb">
-                    <img src={mongoimg} alt="" />
+                    <img src={mongoimg} alt="MongoDB" />
                 </div>
                 <div className="nodejs">
-                    <img src={nodeimg} alt="" />
+                    <img src={nodeimg} alt="Node.js" />
                 </div>
                 <div className="php">
-                    <img src={phpimg} alt="" />
+                    <img src={phpimg} alt="PHP" />
                 </div>
             </div>
-            <div className="thirdrow">
+            <div className="thirdrow hidden" ref={(el) => (rowsRef.current[2] = el)}>
                 <div className="express">
-                    <img src={expressimg} alt="" />
+                    <img src={expressimg} alt="Express" />
                 </div>
                 <div className="js">
-                    <img src={jsimg} alt="" />
+                    <img src={jsimg} alt="JavaScript" />
                 </div>
                 <div className="css">
-                    <img src={cssimg} alt="" />
+                    <img src={cssimg} alt="CSS" />
                 </div>
             </div>
         </div>
